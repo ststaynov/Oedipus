d687e649093fde5fb4425affad74eacb0919d0c1
stoyans <stoyans@fabrique.nl>
2016-04-12 14:48:33 +0200

    smooth animation but lots of events

--- static/js/main.js
+++ static/js/main.js
@@ -20,6 +20,7 @@ function scrolling (s){
     events++;
     ev.text(events);
 
+    //Keep track of the scrolling events
     if(s.originalEvent.wheelDelta /120 > 0) {
         $('.up').text(counterUp);
         counterUp++;
@@ -33,70 +34,33 @@ function scrolling (s){
 }
 
 function moveBackground(f) {
-    var n = parseInt(bg.css('transform').split(',')[4]);
-    console.log("transformation value at moment: " + n);
-
-    if(translateValue < n && n < -7800) {
-        console.log("Return now");
-
-        //get the exact translate value at the moment of jump
-        var n = parseInt(bg.css('transform').split(',')[4]);
-        console.log("transformation value at moment of jump: " + n);
-        var newtranslateValue = -n -8000;
-        translateValue = -newtranslateValue;
-
-        TweenMax.set(bg, {transform: "translate3d(0px, 0px, 0px)"});
-        TweenMax.to(bg, 1, {transform: "translate3d(" + translateValue + "px, 0px, 0px)", onStart:tweenStart});
-
-        console.log("After Jump translatevalue is:  " + translateValue);
-        r = f==="forward" ? translateValue = translateValue + 200 : translateValue = translateValue - 200;
-        return false;
-    } //else if(translateValue > 200) {
-    //    var newtranslateValue = translateValue;
-    //    translateValue = newtranslateValue - 8000;
-    //
-    //    TweenMax.set(bg, {transform: "translate3d(8000px, 0px, 0px)"});
-    //    TweenMax.to(bg, 1, {transform: "translate3d(" + newtranslateValue + "px, 0px, 0px)", onStart:tweenStart});
-    //
-    //    console.log(newtranslateValue);
-    //    return false;
-    //}
-
-    console.log("To be translated to: " + translateValue);
-    TweenMax.to($('.c-brewing-background'), 2, {transform: "translate3d(" + translateValue + "px, 0px, 0px)", onStart:tweenStart});
-
     r = f==="forward" ? translateValue = translateValue + 200 : translateValue = translateValue - 200;
+    TweenMax.to($('.c-brewing-background'), 2, {transform: "translate3d(" + translateValue + "px, 0px, 0px)", onStart:tweenStart, onUpdate:$.throttle( 11580, checkPosition)});
 }
 
 function tweenStart() {
-    startCountdown();
     setTimeout( function(){
         finishedTween();
       }, 2000);
 }
-
+//used to log the tween events at a time
 function finishedTween() {
     events--;
     console.log("Tween Finished");
     ev.text(events);
-
-    var n = parseInt(bg.css('transform').split(',')[4]);
-    if(translateValue < n && n < -7800) {
-        console.log("Return now");
-
-        //get the exact translate value at the moment of jump
-        var newtranslateValue = -translateValue -8200;
-        translateValue = -newtranslateValue - 200;
-        var noscrollTranslation = translateValue;
-
-        TweenMax.set(bg, {transform: "translate3d(" + n+7800 + "px, 0px, 0px)"});
-        TweenMax.to(bg, 1, {transform: "translate3d(" + noscrollTranslation + "px, 0px, 0px)", onStart:tweenStart});
-
-        console.log("After finishedtweetJump translatevalue is:  " + translateValue);
-    }
+    //checkPosition();
 }
 
-function startCountdown() {
-
+function checkPosition() {
+    //Check the current transform value
+    var n = parseInt(bg.css('transform').split(',')[4]);
+    if (n < -7800) {
+        translateValue = n + 8000;
+        TweenMax.set(bg, {transform: "translate3d(" + translateValue + "px, 0px, 0px)"});
+    } else if(n > 200) {
+        translateValue = n - 8000;
+        TweenMax.set(bg, {transform: "translate3d(" + translateValue + "px, 0px, 0px)"});
+    }
+    console.log('One scroll');
 }
 
