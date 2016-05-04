a5a143ce832d90251f0e9e4efb1c500517a788f2
stoyans <stoyans@fabrique.nl>
2016-05-04 17:27:20 +0200

    navigation configuration

--- animation/static/animation/js/main.js
+++ animation/static/animation/js/main.js
@@ -8,8 +8,8 @@ var e = document.body,
     events = 0,
     $bg = $('.c-brewing-background'),
     $fg = $('.c-brewing-foreground'),
-    $btl = $('.c-beer-bottle'),
-    $btlContainer = $('.c-beer-bottle-container'),
+    $btl = $('.c-action'),
+    $btlContainer = $('.c-action-container'),
     $navLine = $('.e-navigation-line'),
     fgBg = [$bg, $fg, $btlContainer],
     $mouse = $('.c-mouse'),
@@ -21,7 +21,7 @@ var e = document.body,
 /* set the transform value with tweenMax at beginning & transform all initial components to their places*/
 TweenMax.set(fgBg, {transform: "translate3d(10px, 0px, 0px)"});
 TweenMax.set($mouse, {transform: "translate3d(2010px, 175px, 0px)"});
-TweenMax.set($millmash, {transform: "translate3d(785px, 70px, 0px)"});
+TweenMax.set($millmash, {transform: "translate3d(790px, 78px, 0px)"});
 TweenMax.set($fl, {transform: "translate3d(1507px, 400px, 0px)"});
 
 
@@ -30,6 +30,7 @@ TweenMax.set($fl, {transform: "translate3d(1507px, 400px, 0px)"});
 $('body').bind('DOMMouseScroll mousewheel', $.throttle( 80, scrolling ));
 $( window ).resize(function() {
     animateNavigation(translateValue);
+    setNavStepPoints();
 });
 
 function scrolling(e) {
@@ -37,13 +38,10 @@ function scrolling(e) {
     events+=2;
     $ev.text(events);
     /* Keep track of the scrolling events */
-    consoleLog(e.originalEvent.wheelDelta);
-    consoleLog(e.originalEvent.detail);
     if(e.originalEvent.wheelDelta /120 > 0 || e.originalEvent.detail < 0) {
         $('.up').text(counterUp);
         counterUp++;
         moveBackground(f="forward");
-        consoleLog(e.originalEvent.wheelDelta);
     }
     else {
         $('.down').text(counterDown);
@@ -56,13 +54,14 @@ function moveBackground(f) {
     r = f==="forward" ? translateValue = translateValue + 200 : translateValue = translateValue - 200;
     animateNavigation(translateValue);
 
-    TweenMax.allTo(fgBg, 2, {transform: "translate3d(" + translateValue + "px, 0px, 0px)", onStart:tweenStart, onUpdate:$.throttle( 1510, checkPosition)});
-    TweenMax.to($btl, 2, {transform: "translate3d(" + -translateValue + "px, 0px, 0px)", onStart:tweenStart, onUpdate:$.throttle( 1510, checkPosition)});
+    TweenMax.allTo(fgBg, 2, {transform: "translate3d(" + translateValue + "px, 0px, 0px)", onStart:tweenStart});
+    TweenMax.to($btl, 2, {transform: "translate3d(" + -translateValue + "px, 0px, 0px)", onStart:tweenStart, onUpdate:$.throttle( 510, checkPosition)});
     displayBgPosition(n);
 }
 
 function checkPosition() {
     /* Check the current transform value */
+    consoleLog('checkPosition;');
     var n = parseInt($bg.css('transform').split(',')[4]);
     if (n < -7800) {
         /* sets new position at the start of the film  */
@@ -144,11 +143,13 @@ function switchDirection() {
     }
 }
 
+var animBreakPoint1 = -300;
+
 function checkbeerBottleState(n) {
-    if (n < -300) {
+    if (n < animBreakPoint1) {
         $btl.addClass('milled-mashed');
     }
-    else if (n > -300) {
+    else if (n > animBreakPoint1) {
         $btl.removeClass('milled-mashed');
     }
 }
@@ -159,29 +160,53 @@ var percentToTravel = 0;
 var roadToTravel = 0;
 var maxroad = 0;
 
+animateNavigation(0);
 function animateNavigation(value) {
     maxroad = parseInt($navLine.css('width'));
     /* calculate what is value from 100% (8000px) ^ calculate what is the road to pass for the percent that we get */
     percentToTravel = parseInt(-value/77);
     roadToTravel = (maxroad/100) * percentToTravel;
-    TweenMax.to($btlnav, 2, {left: roadToTravel + "px", ease: Power0.easeNone});
-    // consoleLog('percentToTravel: ' + percentToTravel);
+    if(roadToTravel < -12) { // Keep nav bottle from overthrowing its endpoint
+        TweenMax.to($btlnav, 2, {left: -12 + "px", ease: Power4.easeNone});
+    } else {
+        TweenMax.to($btlnav, 2, {left: roadToTravel + "px", ease: Power0.easeNone});
+    }// consoleLog('percentToTravel: ' + percentToTravel);
     // consoleLog('translateValue: ' + value);
 }
 
 function btlNavigationSet(translateValue) {
-    if (translateValue < 200) {
-        percentToTravel = parseInt(-translateValue/77);
-        s = (maxroad/100) * percentToTravel ;
-        TweenMax.set($btlnav, {left: s   + "px"});
-    }
-    else if (translateValue >= 200) {
-        percentToTravel = parseInt(-translateValue/77);
-        s = (maxroad/100) * -percentToTravel ;
+    // fores only if it is time to change the film
+    // FIRES ALWAYS
+    percentToTravel = parseInt(-translateValue / 80);
+    s = (maxroad/100) * percentToTravel;
+    consoleLog('set to '+ s);
+    if(s < -12) {
+        TweenMax.set($btlnav, {left: -12 + "px"});
+    } else {
         TweenMax.set($btlnav, {left: s + "px"});
     }
-    // consoleLog('On Value: ' + percentToTravel);
-    // consoleLog('translateValue: ' + translateValue);
+    //Something like this should work
+    // if(s < -12 && s > -30 ) {
+    //     TweenMax.set($btlnav, {left: -12 + "px"});
+    // } else {
+    //     TweenMax.set($btlnav, {left: s + "px"});
+    // }
 }
 
+var $pS1 = $('.e-step-1'),
+    $pS2 = $('.e-step-2'),
+    $pS3 = $('.e-step-3'),
+    $pS4 = $('.e-step-4'),
+    $pS5 = $('.e-step-5'),
+    $points = [$pS1,$pS2,$pS3,$pS4,$pS5];
+
+setNavStepPoints();
+function setNavStepPoints() {
+    var difference = maxroad / 4;
+    var sum = 0;
+    for (i = 0; i < 5; i++){
+        TweenMax.to($points[i], 0, {left: sum + "px", ease: Power0.easeNone});
+        sum = sum + difference;
+    }
+}
 /* NAVIGATION ANIMATIONS END*/
