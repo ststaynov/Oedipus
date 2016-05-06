c42bb8cdfe50240115d95c20a1f79a562d559d34
stoyans <stoyans@fabrique.nl>
2016-05-06 12:03:27 +0200

    From spaghetti to noodle, but working noodle (some navigation calculations, fixed nav when move*)

--- animation/static/animation/js/main.js
+++ animation/static/animation/js/main.js
@@ -53,7 +53,6 @@ function scrolling(e) {
 function moveBackground(f) {
     r = f==="forward" ? translateValue = translateValue + 200 : translateValue = translateValue - 200;
     animateNavigation(translateValue);
-
     TweenMax.allTo(fgBg, 2, {transform: "translate3d(" + translateValue + "px, 0px, 0px)", onStart:tweenStart});
     TweenMax.to($btl, 2, {transform: "translate3d(" + -translateValue + "px, 0px, 0px)", onStart:tweenStart, onUpdate:$.throttle( 510, checkPosition)});
     displayBgPosition(n);
@@ -158,19 +157,28 @@ function checkbeerBottleState(n) {
 /* NAVIGATION ANIMATIONS START */
 var percentToTravel = 0;
 var roadToTravel = 0;
-var maxroad = 0;
+var maxRoad = 0;
+var btlMaxRight = 0;
 
 animateNavigation(0);
 function animateNavigation(value) {
-    maxroad = parseInt($navLine.css('width'));
+    maxRoad = parseInt($navLine.css('width'));
+    btlMaxRight = maxRoad + 7;
+
+    var btlMaxRoad = maxRoad; // keep the long roader so that there is time for the bottle to stay in one position before the change of film
     /* calculate what is value from 100% (8000px) ^ calculate what is the road to pass for the percent that we get */
-    percentToTravel = parseInt(-value/77);
-    roadToTravel = (maxroad/100) * percentToTravel;
-    if(roadToTravel < -12) { // Keep nav bottle from overthrowing its endpoint
-        TweenMax.to($btlnav, 2, {left: -12 + "px", ease: Power4.easeNone});
+    percentToTravel = parseInt(-value / 72);
+    // consoleLog('percentToTravel: ' + percentToTravel + '%');
+    roadToTravel = (btlMaxRoad/100) * percentToTravel;
+    // consoleLog('roadToTravel: ' + roadToTravel + 'px')  ;
+    if(roadToTravel < 11) { // Keep nav bottle from overthrowing its endpoint
+        TweenMax.to($btlnav, 2, {left: 11 + "px", ease: Power4.easeNone});
+    } else if(roadToTravel > btlMaxRight) {
+        TweenMax.to($btlnav, 2, {left: btlMaxRight + "px", ease: Power0.easeNone});
     } else {
         TweenMax.to($btlnav, 2, {left: roadToTravel + "px", ease: Power0.easeNone});
-    }// consoleLog('percentToTravel: ' + percentToTravel);
+    }
+    // consoleLog('percentToTravel: ' + percentToTravel);
     // consoleLog('translateValue: ' + value);
 }
 
@@ -178,10 +186,17 @@ function btlNavigationSet(translateValue) {
     // fores only if it is time to change the film
     // FIRES ALWAYS
     percentToTravel = parseInt(-translateValue / 80);
-    s = (maxroad/100) * percentToTravel;
-    consoleLog('set to '+ s);
-    if(s < -12) {
-        TweenMax.set($btlnav, {left: -12 + "px"});
+    s = (maxRoad/100) * percentToTravel;
+    consoleLog('s: ' + s);
+    consoleLog('btlMaxRight ' + btlMaxRight);
+    consoleLog(translateValue);
+    if(s < 11) {
+        TweenMax.set($btlnav, {left: 11 + "px"});
+    } else if (s < btlMaxRight && s > 960) {
+        consoleLog('set to '+ btlMaxRight);
+        TweenMax.set($btlnav, {left: btlMaxRight + "px"});
+    } else if (translateValue < -5000){ // Some if-else's to check if the film is changing from right-left or from left-right
+        TweenMax.set($btlnav, {left: s + 100 + "px"});
     } else {
         TweenMax.set($btlnav, {left: s + "px"});
     }
@@ -202,7 +217,7 @@ var $pS1 = $('.e-step-1'),
 
 setNavStepPoints();
 function setNavStepPoints() {
-    var difference = maxroad / 4;
+    var difference = maxRoad / 4;
     var sum = 0;
     for (i = 0; i < 5; i++){
         TweenMax.to($points[i], 0, {left: sum + "px", ease: Power0.easeNone});
